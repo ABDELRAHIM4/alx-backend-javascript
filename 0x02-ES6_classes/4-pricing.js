b14ee0b9b4fecfa172d99b60a0bb06fcaad04ed9
ABDELRAHIM4 <abdulrhimsaeed@gmail.com>
2024-07-25 21:34:38 +0000

    classes

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -7,6 +7,29 @@ class Pricing {
 		if (typeof currency !== 'Currency') {
 			throw new TypeError('must be Currency');
 		}
-		this._name = name;
-		thi9s._currency = currency;
+		this._amount = amount;
+		this._currency = currency;
 	}
+	get amount() {
+		return this._amount;
+	}
+	set amount(value) {
+		if (typeof value !== 'number') {
+			throw new TypeError('must be number');
+		}
+		this._amount = value;
+	}
+	get currency() {
+		return this._currency;
+	}
+	set currency (value) {
+		if (typeof value !== 'Currency') {
+			throw new TypeError('must be Currency');
+		}
+		this._currency = value;
+	}
+	displayFullPrice() {
+		return `${this._amount} ${this.currency.displayFullCurrency()}`;
+	}
+}
+export default Pricing;
---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,19 @@
+class Building {
+	constructor (sqft) {
+		if (typeof sqft !== 'number') {
+			throw new TypeError('must be number');
+		}
+		this._sqft = sqft;
+	}
+	get sqft() {
+		return this._sqft;
+	}
+	set sqft(value) {
+		if (typeof value !== 'number') {
+			throw new TypeError('must be number');
+		}
+	}
+	evacuationWarningMessage () {
+		throw new Error('Class extending Building must override evacuationWarningMessage');
+	}
+}
