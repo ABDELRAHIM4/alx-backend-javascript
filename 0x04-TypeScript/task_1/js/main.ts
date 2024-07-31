ea0621bc47db6c1d0b2cdb5ba15ab66073758082
ABDELRAHIM4 <abdulrhimsaeed@gmail.com>
2024-07-31 23:04:32 +0000

    typescript

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,31 @@
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any;
+    }
+  interface Directors extends Teacher {
+    numberOfReports: number;
+    }
+  interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+    }
+  const printTeacher: printTeacherFunction = (firstName: string, lastName: string) => {
+      return `${firstName.charAt(0)}. ${lastName}`;
+      };
+  class StudentClass {
+      private firstname: string;
+      private lastname: string;
+      constructor (firstName: string,lastName: string) {
+        this.firstname = firstName;
+        this.lastname = lastName;
+      }
+      public workOnHomewor() {
+        return ('Currently working');
+      }
+      public displayName() {
+        return (this.firstname);
+      }
+    };
